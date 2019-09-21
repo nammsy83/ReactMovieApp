b932b491945c5a585063853d3876faad896f7fd7
Namrata Subhedar <nammsy@gmail.com>
2019-09-21 17:25:38 +0530

    Updates

---
+++ precache-manifest.bb5ce67f626772cbab9d8df2c47f2947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3c4d73e1f5a9d8a67b1f487c2c7f3d",
+    "url": "/ReactMovieApp/index.html"
+  },
+  {
+    "revision": "cada5a39cd4f910a1c7a",
+    "url": "/ReactMovieApp/static/css/main.3fa081cc.chunk.css"
+  },
+  {
+    "revision": "a06dae52f9c01f8174a5",
+    "url": "/ReactMovieApp/static/js/2.39bbea5e.chunk.js"
+  },
+  {
+    "revision": "cada5a39cd4f910a1c7a",
+    "url": "/ReactMovieApp/static/js/main.a7765141.chunk.js"
+  },
+  {
+    "revision": "00b9081fbe4e5cd8ba01",
+    "url": "/ReactMovieApp/static/js/runtime~main.4fc7ea6b.js"
+  }
+]);
